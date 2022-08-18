1016a5f8b4875ce2702520852fbbefbd26b725b0
Alexander Frank <alexanderf@redandroid.io>
2022-08-18 13:42:34 +0200

    Cleanup in CodeGeneration

--- ksp/src/main/java/io/redandroid/navigator/ksp/CodeGeneration.kt
+++ ksp/src/main/java/io/redandroid/navigator/ksp/CodeGeneration.kt
@@ -9,15 +9,14 @@ private const val PACKAGE = "io.redandroid.navigator"
 private const val NAVIGATOR_COMPOSABLE_NAME = "Navigator"
 
 fun CodeGenerator.generateCode(destinations: List<DestinationDescription>, dependencies: Dependencies) {
-	val navFile = createNewFile(
+	val home = destinations.firstOrNull { it.isHome }?.name ?: error("Couldn't find a ${Destination::class.simpleName} marked as home")
+
+	createNewFile(
 		dependencies = dependencies,
 		packageName = PACKAGE,
 		fileName = NAVIGATOR_COMPOSABLE_NAME
-	)
-
-	val home = destinations.firstOrNull { it.isHome }?.name ?: error("Couldn't find a ${Destination::class.simpleName} marked as home")
-
-	navFile += """
+	).use { navigatorFile ->
+		navigatorFile += """
 package $PACKAGE
 
 import androidx.compose.runtime.Composable
@@ -39,65 +38,66 @@ fun $NAVIGATOR_COMPOSABLE_NAME(
 		screenBuilder.builder()
 		
 ${
-		destinations.joinToString(separator = "\n") { destination ->
-			val params = destination.parameters.joinToString("/") { "{${it.name}}" }
-			val paramSuffix = if (params.isNotBlank()) "/$params" else ""
-			val destinationScreenName = destination.name.replaceFirstChar { it.lowercase(Locale.getDefault()) }
+			destinations.joinToString(separator = "\n") { destination ->
+				val params = destination.parameters.joinToString("/") { "{${it.name}}" }
+				val paramSuffix = if (params.isNotBlank()) "/$params" else ""
+				val destinationScreenName = destination.name.replaceFirstChar { it.lowercase(Locale.getDefault()) }
 
-			listOf(
-				"\t\tcomposable(route = \"${destination.name}$paramSuffix\") {",
-				"\t\t\tscreenBuilder.${destinationScreenName}Composable?.invoke(${destination.name}Context(navHostController, it))",
-				"\t\t}"
-			).joinToString(separator = "\n")
+				listOf(
+					"\t\tcomposable(route = \"${destination.name}$paramSuffix\") {",
+					"\t\t\tscreenBuilder.${destinationScreenName}Composable?.invoke(${destination.name}Context(navHostController, it))",
+					"\t\t}"
+				).joinToString(separator = "\n")
+			}
 		}
-	}
 	}
 }
 		
 class ScreenBuilder {
 ${
-		destinations.joinToString(separator = "\n") { destination ->
-			val destinationScreenName = destination.name.replaceFirstChar { it.lowercase(Locale.getDefault()) }
-			val composableProperty = "${destinationScreenName}Composable"
-			listOf(
-				"\tinternal var $composableProperty: (@Composable ${destination.name}Context.() -> Unit)? = null",
-				"\tfun ${destinationScreenName}Screen(composable: @Composable ${destination.name}Context.() -> Unit) {",
-				"\t\t$composableProperty = composable",
-				"\t}"
-			).joinToString(separator = "\n")
-		}
-	}
-}
-		
-${
-		destinations.joinToString(separator = "\n") { destination ->
-			val params = destination.parameters.joinToString(separator = "\n") { parameter ->
+			destinations.joinToString(separator = "\n") { destination ->
+				val destinationScreenName = destination.name.replaceFirstChar { it.lowercase(Locale.getDefault()) }
+				val composableProperty = "${destinationScreenName}Composable"
 				listOf(
-					"\tval ${parameter.name}: ${parameter.type}",
-					"\t\tget() = navBackStackEntry.arguments?.getString(\"${parameter.name}\")?.to${parameter.type.typeString()}OrNull() ?: error(\"Required parameter ${parameter.name} not provided\")"
-				).joinToString(separator = "\n")
-			}
-
-			val navigations = destination.navigationTargets.joinToString(separator = "\n") { navigation ->
-				val parameterDefinition = navigation.parameters.joinToString { "${it.name}: ${it.type}" }
-				val paramsRoute = navigation.parameters.joinToString(separator = "/") { "\${${it.name}}" }
-				val paramsRouteWithSlash = if (paramsRoute.isNotBlank()) "/$paramsRoute" else ""
-
-				listOf(
-					"\tfun navigateTo${navigation.name}($parameterDefinition) {",
-					"\t\tnavHostController.navigate(\"${navigation.name}$paramsRouteWithSlash\")",
+					"\tinternal var $composableProperty: (@Composable ${destination.name}Context.() -> Unit)? = null",
+					"\tfun ${destinationScreenName}Screen(composable: @Composable ${destination.name}Context.() -> Unit) {",
+					"\t\t$composableProperty = composable",
 					"\t}"
 				).joinToString(separator = "\n")
 			}
-
-
-			listOf(
-				"class ${destination.name}Context(private val navHostController: NavHostController, private val navBackStackEntry: NavBackStackEntry) {",
-				params,
-				navigations,
-				"}"
-			).joinToString(separator = "\n")
 		}
-	}
+}
+		
+${
+			destinations.joinToString(separator = "\n") { destination ->
+				val params = destination.parameters.joinToString(separator = "\n") { parameter ->
+					listOf(
+						"\tval ${parameter.name}: ${parameter.type}",
+						"\t\tget() = navBackStackEntry.arguments?.getString(\"${parameter.name}\")?.to${parameter.type.typeString()}OrNull() ?: error(\"Required parameter ${parameter.name} not provided\")"
+					).joinToString(separator = "\n")
+				}
+
+				val navigations = destination.navigationTargets.joinToString(separator = "\n") { navigation ->
+					val parameterDefinition = navigation.parameters.joinToString { "${it.name}: ${it.type}" }
+					val paramsRoute = navigation.parameters.joinToString(separator = "/") { "\${${it.name}}" }
+					val paramsRouteWithSlash = if (paramsRoute.isNotBlank()) "/$paramsRoute" else ""
+
+					listOf(
+						"\tfun navigateTo${navigation.name}($parameterDefinition) {",
+						"\t\tnavHostController.navigate(\"${navigation.name}$paramsRouteWithSlash\")",
+						"\t}"
+					).joinToString(separator = "\n")
+				}
+
+
+				listOf(
+					"class ${destination.name}Context(private val navHostController: NavHostController, private val navBackStackEntry: NavBackStackEntry) {",
+					params,
+					navigations,
+					"}"
+				).joinToString(separator = "\n")
+			}
+		}
 	""".trimIndent()
+	}
 }
