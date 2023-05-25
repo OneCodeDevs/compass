1f2838430ba2f36233c1b6a6513eb67f95c81a78
Alexander Frank <alexander.frank@onecode.de>
2023-05-25 17:16:43 +0200

    Update demo app to make use of Top destinations

--- demo/app/src/main/kotlin/de/onecode/navigator/demo/MainActivity.kt
+++ demo/app/src/main/kotlin/de/onecode/navigator/demo/MainActivity.kt
@@ -3,26 +3,34 @@ package de.onecode.navigator.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import de.onecode.navigator.demo.details.DetailsScreen
 import de.onecode.navigator.demo.home.HomeScreen
+import de.onecode.navigator.demo.home.SubHomeScreen
 import de.onecode.navigator.demo.ui.theme.NavGraphConfigComposeTheme
 import de.onecode.navigator.demo.wizard.attachWizardSubGraph
 import io.redandroid.navigator.Navigator
+import io.redandroid.navigator.rememberNavigatorController
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			NavGraphConfigComposeTheme {
-				Box(modifier = Modifier.padding(16.dp)) {
-					Main()
-				}
+				Main()
 			}
 		}
 	}
@@ -30,20 +38,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Main() {
-	Scaffold { scaffoldPadding ->
+	val navigatorController = rememberNavigatorController()
+
+	val isHome by navigatorController.currentDestinationIsHome()
+	val isDetails by navigatorController.currentDestinationIsDetails()
+
+	Scaffold(
+		bottomBar = {
+			NavigationBar {
+				NavigationBarItem(
+					selected = isHome,
+					label = { Text(text = "Home") },
+					onClick = { navigatorController.navigateToHome() },
+					icon = {
+						Icon(imageVector = Icons.Default.Home, contentDescription = "")
+					}
+				)
+				NavigationBarItem(
+					selected = isDetails,
+					label = { Text(text = "Wizard") },
+					onClick = { navigatorController.navigateToDetails(Random.nextInt()) },
+					icon = {
+						Icon(imageVector = Icons.Default.List, contentDescription = "")
+					}
+				)
+			}
+		}
+	) { scaffoldPadding ->
 		Navigator(
-			modifier = Modifier.padding(scaffoldPadding)
+			modifier = Modifier
+				.padding(scaffoldPadding)
+				.padding(16.dp),
+			navigatorController = navigatorController
 		) { navGraphBuilder ->
 			homeScreen {
 				HomeScreen(
-					onRandom = ::navigateToDetails,
-					onWizard = ::navigateToWizard
+					onToSub = ::navigateToSubHome
 				)
+			}
+
+			subHomeScreen {
+				SubHomeScreen()
 			}
 
 			detailsScreen {
 				DetailsScreen(
 					number = myParam,
+					onOpenWizard = ::navigateToWizard,
 					onBack = ::popBackStack
 				)
 			}
--- demo/app/src/main/kotlin/de/onecode/navigator/demo/home/HomeDescription.kt
+++ demo/app/src/main/kotlin/de/onecode/navigator/demo/home/HomeDescription.kt
@@ -3,11 +3,11 @@ package de.onecode.navigator.demo.home
 import de.onecode.navigator.api.Destination
 import de.onecode.navigator.api.Home
 import de.onecode.navigator.api.Navigation
-import de.onecode.navigator.demo.details.DetailsDescription
-import de.onecode.navigator.demo.wizard.WizardSubGraph
 
 @Destination(name = "Home")
 @Home
-@Navigation(to = DetailsDescription::class)
-@Navigation(to = WizardSubGraph::class)
+@Navigation(to = SubHomeDestination::class)
 object HomeDescription
+
+@Destination(name = "SubHome")
+object SubHomeDestination
