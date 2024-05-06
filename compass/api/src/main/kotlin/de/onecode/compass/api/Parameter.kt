315f5f9654310a4dfcfd84b87356f8b85a57ebcd
Alexander Frank <alexander.frank@onecode.de>
2024-05-06 09:54:40 +0200

    Restrict optional parameters to String for now

--- compass/api/src/main/kotlin/de/onecode/compass/api/Parameter.kt
+++ compass/api/src/main/kotlin/de/onecode/compass/api/Parameter.kt
@@ -5,4 +5,8 @@ import kotlin.reflect.KClass
 @Repeatable
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
-annotation class Parameter(val name: String, val type: KClass<*>, val required: Boolean = true)
+annotation class Parameter(
+	val name: String,
+	val type: KClass<*>,
+	val required: Boolean = true,
+)
